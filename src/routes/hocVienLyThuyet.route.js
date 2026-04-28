c8bc5473927bc6f64f0867b459dfcb14251eec4c
chienvuong123 <vuongxuanchien247@gmail.com.com>
2026-04-28 09:59:47 +0700

    feat: chi tiet hoc bu ly thuyet

--- src/routes/hocVienLyThuyet.route.js
+++ src/routes/hocVienLyThuyet.route.js
@@ -4,6 +4,7 @@ const ctrl = require("../controllers/hocVienLyThuyet.controller");
 
 router.get("/lop-hoc", ctrl.getDanhSachLop);
 router.get("/hoc-vien/:enrolmentPlanIid", ctrl.getDanhSachHocVien);
+router.get("/hoc-vien/:enrolmentPlanIid/chi-tiet/:studentId", ctrl.getChiTietHocVienLyThuyet);
 router.get(
   "/hoc-vien/khoa/:enrolmentPlanIid/",
   ctrl.getDanhSachHocVienTheoKhoa,
