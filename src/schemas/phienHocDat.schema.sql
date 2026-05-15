549e5b40639c73665fed20a69e21ff7695a749e3
chienvuong123 <vuongxuanchien247@gmail.com.com>
2026-05-15 16:01:35 +0700

    fix: phien hoc dat

--- src/schemas/phienHocDat.schema.sql
+++ src/schemas/phienHocDat.schema.sql
@@ -22,6 +22,74 @@ IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ma_hoc_vien') IS NULL
   ALTER TABLE dbo.phien_hoc_dat ADD ma_hoc_vien NVARCHAR(50) NULL;
 GO
 
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'phien_hoc_id') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD phien_hoc_id INT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ma_khoa') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ma_khoa NVARCHAR(100) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ly_do_tong') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ly_do_tong NVARCHAR(500) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ly_do_td') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ly_do_td NVARCHAR(500) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ly_do_dem') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ly_do_dem NVARCHAR(500) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ly_do_so_san') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ly_do_so_san NVARCHAR(500) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'duyet_tong') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD duyet_tong BIT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'duyet_tu_dong') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD duyet_tu_dong BIT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'duyet_dem') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD duyet_dem BIT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'duyet_so_san') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD duyet_so_san BIT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'id_gv') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD id_gv NVARCHAR(100) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ho_ten_gv') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ho_ten_gv NVARCHAR(255) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'ho_ten_hv') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD ho_ten_hv NVARCHAR(255) NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'thoi_gian_dem') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD thoi_gian_dem FLOAT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'quang_duong_dem') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD quang_duong_dem FLOAT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'tile') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD tile FLOAT NULL;
+GO
+
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'guid_session_id') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD guid_session_id NVARCHAR(200) NULL;
+GO
+
 IF COL_LENGTH(N'dbo.phien_hoc_dat', N'nguoi_thay_doi') IS NULL
   ALTER TABLE dbo.phien_hoc_dat ADD nguoi_thay_doi NVARCHAR(100) NULL;
 GO
@@ -30,6 +98,11 @@ IF COL_LENGTH(N'dbo.phien_hoc_dat', N'thoi_gian_thay_doi') IS NULL
   ALTER TABLE dbo.phien_hoc_dat ADD thoi_gian_thay_doi DATETIME2(0) NULL;
 GO
 
+IF COL_LENGTH(N'dbo.phien_hoc_dat', N'created_at') IS NULL
+  ALTER TABLE dbo.phien_hoc_dat ADD created_at DATETIME2(0) NULL;
+GO
+
 IF COL_LENGTH(N'dbo.phien_hoc_dat', N'updated_at') IS NULL
   ALTER TABLE dbo.phien_hoc_dat ADD updated_at DATETIME2(0) NULL;
 GO
+
